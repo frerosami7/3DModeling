959a9a905c28e8b461398c9461ba2f4c09eb6623
Mohamed <frerosami7@gmail.com>
2017-11-23 15:40:21 +0100

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,6 @@
 (function() {
   var elts = document.getElementsByTagName('p');
   for (var i = 0; i < elts.length; i++) {
-    elts[i].style['background-color'] = '#ffffff'
+    elts[i].style['background-color'] = '#F00';
   }
 })();
